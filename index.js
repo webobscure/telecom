ca9114db33ff179f001223383be542e41ec31e32
Alexandr <sparkygino@gmail.com>
2023-06-01 00:41:36 +0300

    almost finish theme button

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function openPopup() {
    const popup = document.getElementById('popup');
    popup.classList.toggle('active')
 }
+
+function switchTheme() {
+    const themeBox = document.getElementsByClassName('white-theme-box');
+    const box = document.getElementsByClassName('white-theme-settings-box');
+    const icon = document.getElementsByClassName('white-theme__icon');
+    const item = document.getElementsByClassName('item-white');
+    const popupButton = document.getElementsByClassName('white-popup-button') 
+}

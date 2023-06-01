85d12683f5b9158b312425d6a08f5bbc07e48141
Alexandr <50839940+webobscure@users.noreply.github.com>
2023-06-02 00:05:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,3 @@ function openPopup() {
   const popup = document.getElementById('popup');
   popup.classList.toggle('active');
 }
-
-function switchTheme() {
-  let close = document.getElementById('close');
-
-  if (window.matchMedia('(prefers-color-scheme: light)')) {
-    document.body.classList.toggle('dark-theme')
-  } 
-
-  if (document.body.classList.contains('dark-theme')) {
-    close.src = 'assets/dark-close.png';
-  } else {
-    close.src = 'assets/close.png';
-  }
-}
-
-console.log(window.matchMedia('(prefers-color-scheme: dark)'));

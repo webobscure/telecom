03d55552f4bac693285819e1e24b2da014c54cd2
Alexandr <50839940+webobscure@users.noreply.github.com>
2023-06-01 23:43:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 let btn = document.getElementsByClassName('btn');
 
-
 function openPopup() {
-   const popup = document.getElementById('popup');
-   popup.classList.toggle('active')
+  const popup = document.getElementById('popup');
+  popup.classList.toggle('active');
 }
 
 function switchTheme() {
-    let close = document.getElementById("close");
+  let close = document.getElementById('close');
 
-    document.body.classList.toggle("dark-theme")
-    if(document.body.classList.contains("dark-theme")) {
-        close.src = "assets/dark-close.png"
-    } else {
-        close.src = "assets/close.png"
-    }
+  if (window.matchMedia('(prefers-color-scheme: light)')) {
+    document.body.classList.toggle('dark-theme')
+  } 
+
+  if (document.body.classList.contains('dark-theme')) {
+    close.src = 'assets/dark-close.png';
+  } else {
+    close.src = 'assets/close.png';
+  }
 }
+
+console.log(window.matchMedia('(prefers-color-scheme: dark)'));
